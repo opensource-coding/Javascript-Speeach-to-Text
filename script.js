aca6f6e3e1a3f7dabe509c64972652f93d0fc3d3
Open Source Coding <116367908+opensource-coding@users.noreply.github.com>
2022-12-06 21:10:55 +0500

    FINAL

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-const file = document.querySelector("#file"),
-  recordBtn = document.querySelector(".record"),
-  result = document.querySelector(".result");
+const recordBtn = document.querySelector(".record"),
+  result = document.querySelector(".result"),
+  downloadBtn = document.querySelector(".download"),
+  inputLanguage = document.querySelector("#language"),
+  clearBtn = document.querySelector(".clear");
 
-file.addEventListener("change", function () {
-  const reader = new FileReader();
-  reader.addEventListener("load", function () {
-    console.log(reader.result);
-  });
-  reader.readAsText(this.files[0]);
-});
-
-let allow = true,
+let SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition,
+  recognition,
   recording = false;
+
+function populateLanguages() {
+  languages.forEach((lang) => {
+    const option = document.createElement("option");
+    option.value = lang.code;
+    option.innerHTML = lang.name;
+    inputLanguage.appendChild(option);
+  });
+}
+
+populateLanguages();
+
 function speechToText() {
   try {
-    const SpeechRecognition =
-      window.SpeechRecognition || window.webkitSpeechRecognition;
-    const recognition = new SpeechRecognition();
-    recognition.lang = "en-US";
+    recognition = new SpeechRecognition();
+    recognition.lang = inputLanguage.value;
     recognition.interimResults = true;
     2;
-    if (allow) {
-      recognition.start();
-      recording = true;
-    } else {
-      recognition.stop();
-      recording = false;
-      return;
-    }
     recordBtn.classList.add("recording");
     recordBtn.querySelector("p").innerHTML = "Recording...";
+    recognition.start();
     recognition.onresult = (event) => {
       const speechResult = event.results[0][0].transcript;
       //detect when intrim results
@@ -46,6 +45,7 @@ function speechToText() {
         //update the interim p with the speech result
         document.querySelector(".interim").innerHTML = " " + speechResult;
       }
+      downloadBtn.disabled = false;
     };
     recognition.onspeechend = () => {
       recordBtn.querySelector("p").innerHTML = "Start Recording";
@@ -53,25 +53,61 @@ function speechToText() {
       speechToText();
     };
     recognition.onerror = (event) => {
+      recording = false;
       recordBtn.querySelector("p").innerHTML = "Start Recording";
       recordBtn.classList.remove("recording");
       if (event.error === "no-speech") {
-        console.log("No speech was detected. Try again.");
+        alert("No speech was detected. Try again.");
+      } else if (event.error === "audio-capture") {
+        alert(
+          "No microphone was found. Ensure that a microphone is installed."
+        );
+      } else if (event.error === "not-allowed") {
+        alert("Permission to use microphone is blocked.");
+      } else if (event.error === "aborted") {
+        alert("Recording Stopped.");
       } else {
-        console.log("Error occurred in recognition: " + event.error);
+        alert("Error occurred in recognition: " + event.error);
       }
     };
   } catch (error) {
+    recording = false;
+
     console.log(error);
   }
 }
 
 recordBtn.addEventListener("click", () => {
   if (!recording) {
-    allow = true;
     speechToText();
+    recording = true;
   } else {
-    allow = false;
-    speechToText();
+    recognition.stop();
+    recordBtn.querySelector("p").innerHTML = "Start Recording";
+    recordBtn.classList.remove("recording");
+    recording = false;
   }
+});
+
+function download() {
+  const text = result.innerText;
+  const filename = "speech.txt";
+
+  const element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
+  element.style.display = "none";
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
+downloadBtn.addEventListener("click", download);
+
+clearBtn.addEventListener("click", () => {
+  result.innerHTML = "";
+  downloadBtn.disabled = true;
 });
